b9938890ef51fa5c5292c98ee326ba2de62a2765
Mahadi Hasan <87907980+mahadi-xlr8@users.noreply.github.com>
2023-10-03 19:45:02 +0600

    joined date added in signup

--- backend/routes/logins/creator/signup.js
+++ backend/routes/logins/creator/signup.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const app = express.Router();
 const dataCheck = require("../../../middleware/signupDataCheck");
 const { Creator } = require("../../../database");
+const getToday = require("../../../helper/getToday");
 app.post("/", dataCheck, async (req, res) => {
   const SALT_ROUNDS = 10;
   const hash = await bcrypt.hash(req.body.password, SALT_ROUNDS);
@@ -12,16 +13,16 @@ app.post("/", dataCheck, async (req, res) => {
     email: req.body.email,
     profileImg: req.body.profile_photo,
     password: hash,
+    joined: getToday(),
   });
-  try{
-    await creator.save()
+  try {
+    await creator.save();
     res
       .header("x-access-token", creator.genToken())
       .status(200)
       .send("Logged in successfully!");
-  }
-  catch(err){
-    res.status(400).send("Email already exist!")
+  } catch (err) {
+    res.status(400).send("Email already exist!");
   }
 });
 
